16435e62e156dcb2a5c3704d644f4a8d6f49783a
ZhangJian He <shoothzj@gmail.com>
2023-06-11 17:01:20 +0800

    add maven pom file for this project

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,15 @@ repositories {
 }
 
 dependencies {
-    annotationProcessor("org.projectlombok:lombok:1.18.22")
-    compileOnly("org.projectlombok:lombok:1.18.22")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    compileOnly("org.projectlombok:lombok:1.18.28")
     implementation("org.apache.kafka:kafka-clients:3.0.0")
     implementation("io.netty:netty-common:4.1.72.Final")
     implementation("com.google.guava:guava:31.0.1-jre")
     implementation("org.apache.logging.log4j:log4j-core:2.17.0")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.22")
-    testCompileOnly("org.projectlombok:lombok:1.18.22")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
+    testCompileOnly("org.projectlombok:lombok:1.18.28")
     testImplementation("org.apache.kafka:kafka_2.13:3.0.0")
     testImplementation("org.assertj:assertj-core:3.21.0")
     testImplementation("org.mockito:mockito-junit-jupiter:4.2.0")
